1e86b38444f3414ee05a54af0df9c48656b7785e
NaveenPokhriyal <47532433+NaveenPokhriyal@users.noreply.github.com>
2024-07-06 01:24:02 +0530

    initial commit.

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "KMM-One-Time-Password-Library"
+include(":app")
+ 
